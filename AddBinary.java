2cbe18f727d0a86a4d5d1a9022de771c97c0ea14
Sita <gsmadhuri@users.noreply.github.com>
2014-07-18 09:34:20 -0700

    Update and rename AddBinary to AddBinary.java

--- AddBinary.java
+++ AddBinary.java
@@ -6,7 +6,6 @@
 
   //Solution: Starting from the nth character of both strings a & b, add each bit & carry forward any carry
   
-  //Java
   public String addBinary(String a, String b) 
   {
        if(a == null || b == null || a.trim() == "" || b.trim() == "")
@@ -24,23 +23,4 @@
           j--;
       }
       return sum.toString();
-  }
-  
-  
-  //C#
-   public String AddBinary(String a, String b) 
-   {
-      if (string.IsNullOrEmpty(a) || string.IsNullOrEmpty(b))
-        return null;
-      int c = 0,i=a.Length-1 , j = b.Length-1;
-      StringBuilder sum = new StringBuilder();
-      while (i >= 0 || j >= 0 || c > 0)
-      {
-          int ac = i >= 0 ? a[i--] - '0' : 0;
-          int bc = j >= 0 ? b[j--] - '0' : 0;
-          int s = ac + bc + c;
-          sum.Insert(0, s % 2);
-          c = s / 2;
-      }
-      return sum.ToString();
   }
